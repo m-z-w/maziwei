2a74b09937ae015b96b8940f28b585dec7f6f537
maziwei <maziwei@baijiahulian.com>
2016-12-08 17:28:57 +0800

    app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+var a = 0;
+var b = require('./b');
+var $ = require('jquery');
+
+
+console.log($('div'));
+module.exports = b(a);
+
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './bin',
+        publicPath: 'bin/',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /.\js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
