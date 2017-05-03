315907f3100096f1c4db7c88b4a12180c8de6917
maziwei <maziwei@meituan.com>
2017-05-03 15:30:45 +0800

    添加一些依赖

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 var a = 0;
 var b = require('./b');
+var moment = require('moment');
+
+console.log(moment().format());
 console.log(module.paths);
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: './bin',
-        publicPath: 'bin/',
-        filename: 'app.bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
     },
     module: {
         loaders: [{
             test: /.\js$/,
             exclude: /node_modules/,
             loader: 'babel'
-        }]
+        }],
+        plugins: [
+            new ExtractTextPlugin('styles.css'),
+        ]
     }
 };
 
